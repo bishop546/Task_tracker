e8e7d26fb6070a6dd8449c2d2e1f97246d1b7fb4
Isaac <145347871+bishop546@users.noreply.github.com>
2026-09-16 20:47:19 +0100

    Created Task and Category functions as well as added relationships to User function models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, UTC
 from database import Base
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy import Integer, String, Text, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class User(Base):
@@ -11,7 +11,39 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     
-    tasks: Mapped[list["Task"]] = relationship(back_populates="user")
+    tasks: Mapped[list["Task"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+    categories: Mapped[list["Category"]] = relationship(back_populates="user", cascade="all, delete-orphan")
     
 class Task(Base):
-    pass
+    __tablename__ = "tasks"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(20), nullable=False, index=True)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(UTC))
+    due_date: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
+    is_completed: Mapped[bool] = mapped_column(Boolean, default=False)
+    
+    #Foreign keys
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    category_id: Mapped[int | None] = mapped_column(Integer, ForeignKey("categories.id"), nullable=True)
+    
+    #Relationships
+    user: Mapped["User"] = relationship(back_populates="tasks")
+    category: Mapped["Category | None"] = relationship(back_populates="tasks")
+    
+#Think about adding habit model
+
+class Category(Base):
+    __tablename__ = "categories"
+        
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(20), nullable=False, index=True)
+    #color: Mapped[str | None] = mapped_column(String(7), nullable=True) # Optional hex color string
+    
+    #Foreign keys
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    
+    #Relationships
+    user: Mapped["User"] = relationship(back_populates="categories")
+    tasks: Mapped[list["Task"]] = relationship(back_populates="category")    
